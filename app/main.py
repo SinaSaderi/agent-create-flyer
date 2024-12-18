b64cc0ad4279e55a83e36c1a83d7cc9d3badc51b
Sina Saderi <sina.saderi@gmail.com>
2024-12-18 05:58:06 +0330

    change message

--- app/main.py
+++ app/main.py
@@ -1,17 +1,13 @@
 from fastapi import FastAPI
 
-from api.routes.api import router as api_router
-from core.events import create_start_app_handler
-from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
+# Initialize the FastAPI app
+app = FastAPI()
 
-
-def get_application() -> FastAPI:
-    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
-    application.include_router(api_router, prefix=API_PREFIX)
-    pre_load = False
-    if pre_load:
-        application.add_event_handler("startup", create_start_app_handler(application))
-    return application
-
-
-app = get_application()
+# Define a GET endpoint that returns 'Hello World'
+@app.get("/")
+async def read_root():
+    """
+    GET endpoint that returns a simple 'Hello World' message.
+    """
+    return {"message": "Hello World"}
+```
